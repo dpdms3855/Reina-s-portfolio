52164bc4a9a31a2ab79bc7a5050bdd7d1da3f8a8
dpdms3858 <63193046+dpdms3858@users.noreply.github.com>
2020-05-18 23:20:32 +0900

    JS_Navbar_Menu click

--- main.js
+++ main.js
@@ -15,3 +15,18 @@ document.addEventListener('scroll',()=>{
  //스크롤이 될때마다 이 함수를 호출해줘
     //arrow function이라고도하는데, 아무런 인자를 받지않고 우리가 원하는 블럭을 실행하는것
     //스트롤이 될때마다 블럭안에서 작성한 코드가 실행하도록,,,
+
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click',(event)=>{
+    const target= event.target;
+    const link = target.dataset.link;
+    if(link==null){
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+});
