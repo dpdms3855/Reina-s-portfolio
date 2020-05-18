477b88ae40a438021ceca1dd64e26da3c4123d59
dpdms3858 <63193046+dpdms3858@users.noreply.github.com>
2020-05-18 22:49:11 +0900

    JS_Navbar

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//Make navbar transparent when it is on the top
+const navabar = document.querySelector('#navbar');
+const navbarHeight=navbar.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{   
+    console.log(window.scrollY);
+    console.log('navbarHeight: ${navbarHeight}');
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar__dark');
+    }else{
+        navbar.classList.remove('navbar__dark');
+    }
+})
+ //스크롤이 될때마다 이 함수를 호출해줘
+    //arrow function이라고도하는데, 아무런 인자를 받지않고 우리가 원하는 블럭을 실행하는것
+    //스트롤이 될때마다 블럭안에서 작성한 코드가 실행하도록,,,
